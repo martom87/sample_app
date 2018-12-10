b608b68b85ad5ff492bb32e7b039db9f5b6aed89
martom87 <marcintomala87@gmail.com>
2018-12-10 16:13:11 +0100

    contact static page added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+
+
+  end
+
 
 end
